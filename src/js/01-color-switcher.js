01fd344dd257de7d17d231bb38338b517f9d155c
oleg634996 <olegkolodiy22@gmail.com>
2021-11-09 13:17:46 +0200

    v3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('[data-start]')
+const btnStop= document.querySelector('[data-stop]')
+
+let interval = null
+
+btnStart.addEventListener('click', () => {
+    interval = setInterval(() => {
+        document.body.style.background = getRandomHexColor()
+    }, 1000)
+    
+    btnStart.disabled = true
+    btnStop.disabled = false
+});
+  
+btnStop.addEventListener('click', () => {
+    clearInterval(interval)
+
+    btnStart.disabled = false
+    btnStop.disabled = true
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+};
 
